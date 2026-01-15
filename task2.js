4c56503e254a227b5cff61b5792abfd915d2d361
Leslie Rivera <capuchina.lr@gmail.com>
2026-01-14 22:06:29 -0600

    Task resueltas de laboratorio

--- task2.js
+++ task2.js
@@ -1,2 +1,10 @@
 // Task 2: listUsers()
+import { getServerURL } from "./task1.js";
 
+export function listUsers() {
+        return fetch(getServerURL() + "/users")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        });
+}
